ec8f85977e8a2f853e89e10b2fc067bc46d655aa
Tom Lee <github@tomlee.co>
2012-04-23 23:12:19 +0000

    Further simplify the example code.

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,39 +1,16 @@
 #include <Application.h>
 #include <Window.h>
 
-class MainWindow : public BWindow
-{
-	public:
-		MainWindow();
-		virtual ~MainWindow();
-		
-	protected:
-		void MessageReceived(BMessage *msg);
-};
-
-MainWindow::MainWindow()
-	: BWindow(BRect(50, 50, 320, 200), "Hello World",
-		B_TITLED_WINDOW, B_ASYNCHRONOUS_CONTROLS | B_QUIT_ON_WINDOW_CLOSE)
-{
-}
-
-MainWindow::~MainWindow()
-{
-}
-
-void
-MainWindow::MessageReceived(BMessage *msg)
-{
-	
-}
-
 int
 main(int argc, char **argv)
 {
 	BApplication *app =
 		new BApplication("application/x-vnd.example-helloworld");
-		
-	MainWindow *window = new MainWindow();
+			
+	const window_type type = B_TITLED_WINDOW;
+	const uint32 flags = B_ASYNCHRONOUS_CONTROLS | B_QUIT_ON_WINDOW_CLOSE;
+	BWindow *window =
+		new BWindow(BRect(50, 50, 320, 200), "Hello World", type, flags);
 	window->Show();
 	
 	app->Run();
